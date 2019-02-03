9773ee0933f64c25b13a44d845c5c1968b90a089
Mark Chavez <markchav3z@gmail.com>
2019-02-03 18:05:12 +0800

    Update Gemfile.lock and add tests

--- spec/github_client_spec.rb
+++ spec/github_client_spec.rb
@@ -2,4 +2,14 @@ RSpec.describe GithubClient do
   it "has a version number" do
     expect(GithubClient::VERSION).not_to be nil
   end
+
+  it "allows setting of host" do
+    client = described_class.new(host: "sample host")
+    expect(client.host).to eq "sample host"
+  end
+
+  it "allows setting of access_token" do
+    client = described_class.new(access_token: "sample access_token")
+    expect(client.access_token).to eq "sample access_token"
+  end
 end
