53392f160aec1cd78ff41dce91fab653e0d41a7b
Mark Chavez <markchav3z@gmail.com>
2019-02-03 18:09:46 +0800

    Require uri and json

--- lib/github_client.rb
+++ lib/github_client.rb
@@ -1,10 +1,7 @@
 require "github_client/version"
 require "api_client_base"
-require "github_client/client"
-require "github_client/requests/get_user_repositories_request"
-require "github_client/requests/get_org_repositories_request"
-require "github_client/responses/get_user_repositories_response"
-require "github_client/responses/get_org_repositories_response"
+require "uri"
+require "json"
 
 module GithubClient
   include APIClientBase::Base
@@ -14,3 +11,9 @@ module GithubClient
     has :access_token, classes: String
   end
 end
+
+require "github_client/client"
+require "github_client/requests/get_user_repositories_request"
+require "github_client/requests/get_org_repositories_request"
+require "github_client/responses/get_user_repositories_response"
+require "github_client/responses/get_org_repositories_response"
